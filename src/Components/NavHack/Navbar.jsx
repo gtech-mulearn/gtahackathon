9b8e5af9bdf6790ec24e39f52dab0cea69888439
Amal C P <acp070146@gmail.com>
2023-08-31 16:40:57 +0530

    sandshore section starts

---
+++ src/Components/NavHack/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { GTALogo } from "../../assets/svg/svg.tsx";
+import styles from "./Navbar.module.css";
+import { useReactPath } from "./path.hook.js";
+
+const Navbar = () => {
+  const [openmenu, setopenmenu] = useState(false);
+  const [navbg, setNavBg] = useState(false);
+  function openMenu() {
+    setopenmenu(!openmenu);
+  }
+  const path = useReactPath();
+  const navContent = [
+    "Overview",
+    "The-Series",
+    "Why-GTA?",
+    "Explore",
+    "Support",
+  ];
+  useEffect(() => {}, [path]);
+  const changeNavBg = () => {
+    window.scrollY >= 150 ? setNavBg(true) : setNavBg(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavBg);
+    return () => {
+      window.removeEventListener("scroll", changeNavBg);
+    };
+  }, []);
+  return (
+    <div
+      className={styles.navbarWrapper}
+      style={{
+        background: navbg ? "rgba(255,255,255,0.4)" : "rgba(0, 0, 0, 0.85)",
+      }}
+    >
+      <a href="/" className={styles.navLogoWrapper}>
+        <GTALogo />
+      </a>
+      <div className={styles.navigationContentWrapper}>
+        {" "}
+        {navContent.map((content, i) => (
+          <a
+            href={`#${content}`}
+            key={i.toString() + content}
+            className={styles.atagWrapper}
+          >
+            <p
+              style={{
+                borderBottom: window.location.href.includes(`#${content}`)
+                  ? "4px solid #fff"
+                  : "",
+                padding: "10px 5px 20px",
+                margin: "0px 0px",
+                color: window.location.href.includes(`#${content}`)
+                  ? "#fff"
+                  : "#E8E8E8",
+                fontWeight: window.location.href.includes(`#${content}`)
+                  ? 700
+                  : 400,
+              }}
+            >
+              {content}
+            </p>
+          </a>
+        ))}
+      </div>
+      <a href="" className={styles.navRegisterButton}>
+        REGISTER
+      </a>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/HackathonPages/GTASandshore/GTASandshore.jsx
+++ src/HackathonPages/GTASandshore/GTASandshore.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Home from "./Components/Home/Home";
+import NavHack from "../../Components/NavHack/Navbar";
 
 const GTASandshore = () => {
   return (
     <div>
-      
+      <NavHack />
     </div>
-  )
-}
+  );
+};
 
-export default GTASandshore
+export default GTASandshore;
