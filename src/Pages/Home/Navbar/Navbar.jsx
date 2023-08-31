232f952a3eb06323c0ca89350e76bef42a771dc0
Amal C P <acp070146@gmail.com>
2023-08-31 12:44:17 +0530

    navbar updates

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import {ULearn ,GTALogo} from '../../../assets/svg/svg'
-import styles from './Navbar.module.css'
+import React, { useEffect, useState } from "react";
+import { ULearn, GTALogo } from "../../../assets/svg/svg.tsx";
+import styles from "./Navbar.module.css";
+import { useReactPath } from "./path.hook.js";
 
 const Navbar = () => {
+  const [openmenu, setopenmenu] = useState(false);
+  const [navbg, setNavBg] = useState(false);
+  function openMenu() {
+    setopenmenu(!openmenu);
+  }
+  const path = useReactPath();
+  const navContent = ["O", "about", "team", "contact"];
+  useEffect(() => {}, [path]);
+  const changeNavBg = () => {
+    window.scrollY >= 150 ? setNavBg(true) : setNavBg(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavBg);
+    return () => {
+      window.removeEventListener("scroll", changeNavBg);
+    };
+  }, []);
   return (
     <div className={styles.navbarWrapper}>
-      <div></div>
+      <div className={styles.navLogoWrapper}>
+        <ULearn />
+        <div></div>
+        <GTALogo />
+      </div>
       <div></div>
       <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
