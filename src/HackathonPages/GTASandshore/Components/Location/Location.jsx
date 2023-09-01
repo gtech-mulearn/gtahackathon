71f4c1fe2c622c1e1ae12075b7c5b0100177c5d6
Amal C P <acp070146@gmail.com>
2023-09-01 09:51:06 +0530

    location section updated

--- src/HackathonPages/GTASandshore/Components/Location/Location.jsx
+++ src/HackathonPages/GTASandshore/Components/Location/Location.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import styles from "./Location.module.css";
+import venueLogo from "./assets/venueLogo.png";
+import card from "./assets/card.png";
 
 const Location = () => {
   return (
-    <div>
-      
+    <div className={styles.LocationCard}>
+      <div className={styles.locationWrapper}>
+        <div className={styles.infoContainer}>
+          <img src={venueLogo} alt="" />
+          <div className={styles.infosection}>
+            <h1>Hackathon Venue</h1>
+            <p>
+              ABIDEEPS PARADISE,<br></br>Pockanchery, Vadanappally,<br></br>
+              Kerala - India 680619
+            </p>
+          </div>
+          <a
+            href="https://goo.gl/maps/S12dPAnw33JfFamd6"
+            target="_blank"
+            className={styles.atagButton}
+          >
+            EXPLORE
+          </a>
+        </div>
+      </div>
+      <div className={styles.cardWrapper}>
+        <img src={card} alt="" />
+        <div>
+          <p>
+            Beach vibe, 24 hours of<br></br>Coding hackathon, games &<br></br>
+            events, Absolutely free of
+            <br></br>cost, Food and Beverages,<br></br>Snacks and more.
+          </p>
+          <div></div>
+          <p>Hackathon Goodies, Karma<br></br>Points</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Location
+export default Location;
