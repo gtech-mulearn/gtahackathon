7effe098b75dac0c42d3e054fa528d9e74572be2
Amal C P <acp070146@gmail.com>
2023-09-01 16:35:12 +0530

    loader added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Landing from "./Landing/Landing";
 import styles from './Home.module.css'
+import GTASandshores from '../../HackathonPages/GTASandshore/GTASandshore'
 
 const Home = () => {
   return (
     <div className={styles.homeWrapper}>
-      <Navbar />
-      <Landing />
+      {/* <Navbar />
+      <Landing /> */}
+      <GTASandshores />
     </div>
   );
 };
