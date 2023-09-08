2067831c942f1d29a5483730df97c45a74428724
Aswin Asok <aswinasokofficial@gmail.com>
2023-09-08 19:54:20 +0530

    Update About.jsx

--- src/HackathonPages/GTASandshore/Components/About/About.jsx
+++ src/HackathonPages/GTASandshore/Components/About/About.jsx
@@ -14,8 +14,8 @@ const About = () => {
       <div className={styles.rightContainer}>
         <div>
           <img src={star} alt="" />
-          <h1>14-15</h1>
-          <h2>OCTOBER 2023</h2>
+          <h1>11-12</h1>
+          <h2>NOVEMBER 2023</h2>
           <PinkShade />
         </div>
         <div>
