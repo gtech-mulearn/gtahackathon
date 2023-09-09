e53f3621819b510a0f9b148079973cf96aaee974
Amal C P <acp070146@gmail.com>
2023-09-09 16:54:03 +0530

    why gta

--- src/Pages/Home/WhyGTA/WhyGTA.jsx
+++ src/Pages/Home/WhyGTA/WhyGTA.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import styles from './WhyGTA.module.css'
+import React from "react";
+import styles from "./WhyGTA.module.css";
+import image from "./assets/image.svg";
 const WhyGTA = () => {
   return (
-    <div>
-      whygta
+    <div id="Overview" className={styles.OverviewWrapper}>
+      <div className={styles.InnerDiv}>
+        <img src={image} alt="" />
+        <div>
+          <h1>Why GTA Hackathon Series?</h1>
+          <ul>
+            <li>
+              Unleash your creativity and problem-solving skills in a
+              tech-fueled environment.
+            </li>
+            <li>Forge invaluable connections with industry giants.</li>
+            <li>Push your limits in a 24-hour format.</li>
+            <li>
+              Connect with potential employers, mentors, and fellow tech
+              enthusiasts.
+            </li>
+            <li>GTA empowers youth by promoting skill development.</li>
+            <li>
+              Showcase your talents, tackle real-world challenges, and build
+              lasting connections.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhyGTA
+export default WhyGTA;
