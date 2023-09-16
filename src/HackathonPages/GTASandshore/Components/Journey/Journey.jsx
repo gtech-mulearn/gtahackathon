a769501597d13e1ba2b075e92af56651f34b9f15
Amal C P <acp070146@gmail.com>
2023-09-16 16:27:42 +0530

    journey updated

--- src/HackathonPages/GTASandshore/Components/Journey/Journey.jsx
+++ src/HackathonPages/GTASandshore/Components/Journey/Journey.jsx
@@ -6,7 +6,8 @@ import {
   CompusSvg,
   ComputerSvg,
 } from "../../../../assets/svg/svg.tsx";
-import Scissors from "./assets/Scissor.png";
+
+import { PrizeSvg, PartySvg,TimerSvg } from "./assets/svg.tsx";
 import Check from "./assets/Checkpoint.png";
 const Journey = () => {
   return (
@@ -16,7 +17,7 @@ const Journey = () => {
         <div className={styles.mapWrapper}>
           <img className={styles.background} src={map} alt="" />
           <div className={styles.maskbg}></div>
-          {/* <div className={styles.timelineWrapper}>
+          <div className={styles.timelineWrapper}>
             <div className={styles.container + " " + styles.first}>
               <div className={styles.yellowCircle}></div>
               <PersonSvg />
@@ -26,10 +27,11 @@ const Journey = () => {
               </div>
             </div>
             <div className={styles.strip1}></div>
+            <div className={styles.strip1Sub}></div>
 
             <div className={styles.container + " " + styles.second}>
               <div className={styles.yellowCircle}></div>
-              <PersonSvg />
+              <TimerSvg />
               <div className={styles.inner}>
                 <h3>October 20</h3>
                 <p>Registration Ends</p>
@@ -41,7 +43,7 @@ const Journey = () => {
 
             <div className={styles.container + " " + styles.third}>
               <div className={styles.yellowCircle}></div>
-              <PersonSvg />
+              <ComputerSvg />
               <div className={styles.inner}>
                 <h3>October 22</h3>
                 <p>First Shortlisting</p>
@@ -49,42 +51,51 @@ const Journey = () => {
             </div>
             <div className={styles.strip3}></div>
             <div className={styles.strip3Sub}></div>
-            <div className={styles.container}>
-              <div className={styles.yellowCircle}></div>
-              <PersonSvg />
+
+            <div className={styles.container + " " + styles.four}>
               <div className={styles.inner}>
                 <h3>September 01</h3>
                 <p>Shortlisting Process Ends</p>
               </div>
+              <CompusSvg />
+
+              <div className={styles.yellowCircle}></div>
             </div>
             <div className={styles.strip4}></div>
             <div className={styles.strip4Sub}></div>
 
-            <div className={styles.container}>
-              <div className={styles.yellowCircle}></div>
-              <PersonSvg />
+            <div className={styles.container + " " + styles.five}>
               <div className={styles.inner}>
                 <h3>September 01</h3>
-                <p>Shortlisting Teams Announced</p>
+                <p>Shortlisted teams announced</p>
               </div>
-            </div>
-            <div className={styles.container}>
+              <img src={Check} alt="" />
+
               <div className={styles.yellowCircle}></div>
-              <PersonSvg />
+            </div>
+            <div className={styles.strip5}></div>
+
+            <div className={styles.container + " " + styles.six}>
               <div className={styles.inner}>
                 <h3>November 11</h3>
                 <p>Hackathon Starts</p>
               </div>
-            </div>
-            <div className={styles.container}>
+              <PartySvg />
+
               <div className={styles.yellowCircle}></div>
-              <PersonSvg />
+            </div>
+            <div className={styles.strip6}></div>
+            <div className={styles.strip6Sub}></div>
+
+            <div className={styles.container + " " + styles.seven}>
+              <div className={styles.yellowCircle}></div>
+              <PrizeSvg />
               <div className={styles.inner}>
                 <h3>November 12</h3>
                 <p>Hackathon Ends</p>
               </div>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
