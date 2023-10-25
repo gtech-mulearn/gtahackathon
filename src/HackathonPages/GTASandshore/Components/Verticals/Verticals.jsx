7d5a2e92950b92c63c17bea6be710c78a85aa138
Jenin82 <jenin8282@gmail.com>
2023-10-25 18:08:43 +0530

    feat(): problem statements added for phase one

--- src/HackathonPages/GTASandshore/Components/Verticals/Verticals.jsx
+++ src/HackathonPages/GTASandshore/Components/Verticals/Verticals.jsx
@@ -5,35 +5,44 @@ import Aiml from "./assets/aiml.svg";
 import web from "./assets/website.svg";
 import Appli from "./assets/application.svg";
 const Verticals = () => {
-  return (
-    <>
-      <div className={styles.verticalWrapper}>
-        <h1>Our Verticals</h1>
-        <div className={styles.contentContainer}>
-          <div>
-            <img src={Aiml} alt="" loading="lazy" />
-          </div>
-          <div>
-            <img src={arvr} alt="" loading="lazy" />
-          </div>
-          <div>
-            <img src={web} alt="" loading="lazy" />
-          </div>
-          <div>
-            <img src={Appli} alt="" loading="lazy" />
-          </div>
-        </div>
-      </div>
-      <div className={styles.verticalWrapper}>
-        <h1>Problem Stetements</h1>
-        <div className={styles.contentContainer + " " + styles.problem}>
-          <a href="">AI / ML</a>
-          <a href="">AR / VR</a>
-          <a href="">WEBSITE</a> <a href="">APPLICATION</a>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.verticalWrapper}>
+                <h1>Our Verticals</h1>
+                <div className={styles.contentContainer}>
+                    <div>
+                        <img src={Aiml} alt="" loading="lazy" />
+                    </div>
+                    <div>
+                        <img src={arvr} alt="" loading="lazy" />
+                    </div>
+                    <div>
+                        <img src={web} alt="" loading="lazy" />
+                    </div>
+                    <div>
+                        <img src={Appli} alt="" loading="lazy" />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.verticalWrapper}>
+                <h1>Problem Stetements</h1>
+                <div className={styles.contentContainer + " " + styles.problem}>
+                    <a href="https://mulearnfoundation.notion.site/AI-ML-fa133a33fe93439ba718d0294037564a">
+                        AI / ML
+                    </a>
+                    <a href="https://mulearnfoundation.notion.site/AR-VR-7ffccc2649af4f7ca4aeded12c0a5b5a">
+                        AR / VR
+                    </a>
+                    <a href="https://mulearnfoundation.notion.site/WEB-and-APP-69d350902d8c4b54b38458564f52aaf8">
+                        WEBSITE
+                    </a>
+                    <a href="https://mulearnfoundation.notion.site/WEB-and-APP-69d350902d8c4b54b38458564f52aaf8">
+                        APPLICATION
+                    </a>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Verticals;
